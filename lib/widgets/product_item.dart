9a84e734ed01d2cd20f0342d25ff1b694320c63e
Matthew Gleich <matthewgleich@gmail.com>
2020-04-06 23:37:21 -0400

    🚀 Add snack bar

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -38,8 +38,24 @@ class ProductItem extends StatelessWidget {
           trailing: IconButton(
             icon: Icon(Icons.shopping_cart),
             color: Theme.of(context).accentColor,
-            onPressed: () =>
-                cart.addItem(product.id, product.price, product.title),
+            onPressed: () {
+              cart.addItem(product.id, product.price, product.title);
+              Scaffold.of(context).hideCurrentSnackBar();
+              Scaffold.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(
+                    "Added Item to Cart",
+                  ),
+                  duration: const Duration(seconds: 2),
+                  action: SnackBarAction(
+                    label: "UNDO",
+                    onPressed: () {
+                      cart.removeSingleItem(product.id);
+                    },
+                  ),
+                ),
+              );
+            },
           ),
           backgroundColor: Colors.black87,
           title: Text(
