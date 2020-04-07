be846103922d0d70be456a55d982e2579019ee7e
Matthew Gleich <matthewgleich@gmail.com>
2020-04-07 13:52:02 -0400

    🚀 Add confirmation dialog in cart

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -34,6 +34,27 @@ class CartItem extends StatelessWidget {
       onDismissed: (_) {
         Provider.of<Cart>(context, listen: false).removeItem(productId);
       },
+      confirmDismiss: (direction) {
+        return showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: Text("Are you sure?"),
+            content: Text(
+              "Do you want to remove the item from the cart",
+            ),
+            actions: <Widget>[
+              FlatButton(
+                onPressed: () => Navigator.of(ctx).pop(true),
+                child: Text("Yes"),
+              ),
+              FlatButton(
+                onPressed: () => Navigator.of(ctx).pop(false),
+                child: Text("No"),
+              ),
+            ],
+          ),
+        );
+      },
       child: Card(
         margin: EdgeInsets.symmetric(
           horizontal: 15,
